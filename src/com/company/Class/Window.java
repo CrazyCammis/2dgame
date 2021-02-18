5000f3e39ddbdbde257b9d25e8bedba57aef3a64
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2021-02-18 17:19:39 +0100

    added api desription file

--- src/com/company/Class/Window.java
+++ src/com/company/Class/Window.java
@@ -1,50 +1,50 @@
-//package com.company.Class;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.image.BufferStrategy;
-//import java.awt.image.BufferedImage;
-//
-//public class Window {
-//    private JFrame frame;
-//    private BufferedImage image;
-//    private Canvas canvas;
-//    private BufferStrategy bufferStrategy;
-//    private Graphics graphics;
-//
-//
-//
-//    public Window(GameContainer gameContainer){
-//        image = new BufferedImage(gameContainer.getWidth(), gameContainer.getHeight(), BufferedImage.TYPE_INT_RGB);
-//        canvas = new Canvas();
-//        Dimension s = new Dimension(gameContainer.getWidth() * gameContainer.getScale(), gameContainer.getHeight() * gameContainer.getScale());
-//        canvas.setPreferredSize(s);
-//        canvas.setMaximumSize(s);
-//        canvas.setMinimumSize(s);
-//
-//        frame = new JFrame(gameContainer.getTitle());
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setLayout(new BorderLayout());
-//        frame.add(canvas, BorderLayout.CENTER);
-//        frame.pack();
-//        frame.setLocationRelativeTo(null);
-//        frame.setResizable(false);
-//        frame.setVisible(true);
-//
-//        canvas.createBufferStrategy(2);
-//        bufferStrategy = canvas.getBufferStrategy();
-//        graphics = bufferStrategy.getDrawGraphics();
-//    }
-//    public void update(){
-//        graphics.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(), null);
-//        bufferStrategy.show();
-//    }
-//
-//    public BufferedImage getImage() {
-//        return image;
-//    }
-//
-//    public Canvas getCanvas() {
-//        return canvas;
-//    }
-//}
+package com.company.Class;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+public class Window {
+    private JFrame frame;
+    private BufferedImage image;
+    private Canvas canvas;
+    private BufferStrategy bufferStrategy;
+    private Graphics graphics;
+
+
+
+    public Window(GameContainer gameContainer){
+        image = new BufferedImage(gameContainer.getWidth(), gameContainer.getHeight(), BufferedImage.TYPE_INT_RGB);
+        canvas = new Canvas();
+        Dimension s = new Dimension(gameContainer.getWidth() * gameContainer.getScale(), gameContainer.getHeight() * gameContainer.getScale());
+        canvas.setPreferredSize(s);
+        canvas.setMaximumSize(s);
+        canvas.setMinimumSize(s);
+
+        frame = new JFrame(gameContainer.getTitle());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(canvas, BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        graphics = bufferStrategy.getDrawGraphics();
+    }
+    public void update(){
+        graphics.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(), null);
+        bufferStrategy.show();
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+}
