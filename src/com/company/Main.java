ed728f3cfdc4f0ffe0f57a337f6e7c59b118facd
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2021-02-18 15:28:10 +0100

    !

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("ITS ALIVE");
+        System.out.println("SATANS I HELVETE FITTTE BALLE");
     }
 }
