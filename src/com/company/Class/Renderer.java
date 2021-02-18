a405adf327679343d890115ef121e2ed950f2789
Levi <leventen@hiof.no>
2021-02-18 16:29:35 +0100

    Added Window.java and Renderer.java

---
+++ src/com/company/Class/Renderer.java
@@ -0,0 +1,21 @@
+package com.company.Class;
+
+import java.awt.image.DataBufferInt;
+
+public class Renderer {
+    private int pixelWidth, pixelHeight;
+    private int[] p;
+
+    public Renderer(GameContainer gameContainer){
+        pixelWidth = gameContainer.getWidth();
+        pixelHeight = gameContainer.getHeight();
+        // gives p[] direct access to the pixel data of our image in our window
+        p = ((DataBufferInt) gameContainer.getWindow().getImage().getRaster().getDataBuffer()).getData();
+    }
+
+    public void clear(){
+        for(int i = 0; i < p.length; i++){
+            p[i] = 0;
+        }
+    }
+}
